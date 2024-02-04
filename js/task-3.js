a3b471e3ab6dc06223af93f2900daf75cd7dfbe7
Makaka449 <145019010+Makaka449@users.noreply.github.com>
2024-02-04 17:18:53 +0200

    .

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,24 @@
-function getElementWidth(content,padding,border) {
-    content = Number.parseFloat(content)
-    padding = Number.parseFloat(padding)
-    border = Number.parseFloat(border)
-    return content + padding * 2 + border * 2
+function checkForSpam(message) {
+    // Перевіряємо, чи рядок містить заборонені слова
+    const сaseMessage = message.toLowerCase();
     
+    switch (true) {
+        case сaseMessage.includes("spam"):
+        case сaseMessage.includes("sale"):
+            return true;
+        default:
+            return false;
+    }
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+
+ 
